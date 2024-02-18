c894a60e54ade2d1ab9492c1d59b22f6ce6e1d94
sola <dariuslewiss@hotmail.co.uk>
2024-02-18 21:03:55 +0000

    update

--- src/components/CustomMarker.js
+++ src/components/CustomMarker.js
@@ -55,7 +55,7 @@ const CustomMarker = ({ position, onClick, colors, index, mediaItemsLength, city
     // Replace with nrm.png
     //const htmlSvg = `<div class="marker-wrapper"><svg width="${size}" height="${size}" viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg"> <path d="M100.106 0L115 54.1591L158.884 19.0983L139.1 71.6687L195.211 69.0983L148.306 100L195.211 130.902L139.1 128.331L158.884 180.902L115 145.841L100.106 200L85.211 145.841L41.3271 180.902L61.111 128.331L5 130.902L51.9057 100L5 69.0983L61.111 71.6687L41.3271 19.0983L85.211 54.1591L100.106 0Z" fill="${colorToUse}" /> </svg></div>`;
     
-    const htmlPng = `<div class="marker-wrapper"><img class="marker-image"src="nrm5/nrmb.png" width="${size}" height="${size}" /></div>`;
+    const htmlPng = `<div class="marker-wrapper"><img class="marker-image"src="/nrmb.png" width="${size}" height="${size}" /></div>`;
     const htmlWithTintOverlay = `<div class="marker-tint-overlay">${htmlPng}</div>`;
     const newIcon = new L.DivIcon({
       className: 'custom-marker',
